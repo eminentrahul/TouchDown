6800e9691faa50a9e0fbeb2848659eb0d8797dee
Rahul Ravi Prakash <eminentrahul@rocketmaiil.com>
2021-12-18 00:01:55 +0530

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Rahul Ravi Prakash on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
