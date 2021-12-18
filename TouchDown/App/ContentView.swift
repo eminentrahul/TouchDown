014a705373f7a6f4a3c22ebad094a2d54317f59d
Rahul Ravi Prakash <eminentrahul@rocketmaiil.com>
2021-12-18 15:57:03 +0530

    FeaturedTabView done.

--- TouchDown/App/ContentView.swift
+++ TouchDown/App/ContentView.swift
@@ -16,13 +16,24 @@ struct ContentView: View {
             VStack(spacing: 0) {
                 NavigationBarView()
                     .padding(.horizontal, 15)
-                    .padding()
+                    .padding(.bottom)
                     .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
                     .background(.white)
                     .shadow(color: .black.opacity(0.05), radius:5, x: 0, y: 5)
-                Spacer()
-                FooterView()
-                    .padding(.horizontal)
+                
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack(spacing: 0) {
+                        
+                        FeaturedTabView()
+                            .padding(.vertical, 20)
+                            .frame(height: UIScreen.main.bounds.width / 1.475)
+                        
+                        FooterView()
+                            .padding(.horizontal)
+                        
+                    }//:VStack
+                })//:Scroll View
+                
             }//:VStack
             .background(colorBackground.ignoresSafeArea(.all, edges: .all))
         }//:Zstack
---
+++ TouchDown/View/Home/FeaturedItemView.swift
@@ -0,0 +1,31 @@
+//
+//  FeaturedItemView.swift
+//  TouchDown
+//
+//  Created by Rahul Ravi Prakash on 18/12/21.
+//
+
+import SwiftUI
+
+struct FeaturedItemView: View {
+    //MARK: - Properties
+    let player: Player
+    
+    //MARK: - Body
+    var body: some View {
+        Image(player.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+//MARK: - Preview
+struct FeaturedItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedItemView(player: players[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}
